738b78f17c4e8acffa88dfde865b68d8ba04c3ec
lcdantechen <lcdantechen@gmail.com>
2017-03-05 22:57:44 -0500

    update

---
+++ SwiftPlayGround/SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("chenliu")
+
+let otherStr = "Hi Chen" // let string cannot be changed
+
+//strings
+
+var Name = "ChenLiu"
