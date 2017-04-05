f3c0a24f2ceb31817f9914eadaa5983c3b955e27
lcdantechen <lcdantechen@gmail.com>
2017-04-04 20:25:19 -0400

    update

---
+++ IsItPrime/SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+// prime
+
+import UIKit
+
+let number = 17
+
+var isPrime = true
+
+if number == 1 {
+    isPrime = false
+}
+
+var i=2
+
+while i < number {
+    if number % i == 0 {
+        isPrime  = false
+    }
+    
+    i += 1
+}
+
+print(isPrime)
+
+
+
+
+
+
+
