ac10afc33e82e531b79bfcdf26aa072e29c67c66
lcdantechen <lcdantechen@gmail.com>
2017-04-04 21:06:08 -0400

    update

--- IsItPrime/Is It Prime/Is It Prime/ViewController.swift
+++ IsItPrime/Is It Prime/Is It Prime/ViewController.swift
@@ -12,6 +12,41 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var numberTextField: UITextField!
     @IBAction func isItPrime(_ sender: Any) {
+        
+        if let userEnteredString = numberTextField.text {
+            
+            let userEnteredInteger = Int(userEnteredString)
+            
+            if let number = userEnteredInteger {
+                
+                var isPrime = true
+                
+                if number == 1 {
+                    isPrime = false
+                }
+                
+                var i=2
+                
+                while i < number {
+                    if number % i == 0 {
+                        isPrime  = false
+                    }
+                    
+                    i += 1
+                }
+                
+                if isPrime {
+                    resultLabel.text = "\(number) is prime!"
+                } else {
+                    resultLabel.text = "\(number) is not prime"
+                }
+                
+            } else {
+                resultLabel.text = "Please enter a positive whole number"
+            }
+        }
+        
+        
     }
     @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
